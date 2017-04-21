ebe59fbb7f14289d142beb29b09645628bf37815
gfleury <gfleury@ip-172-31-13-97.us-west-2.compute.internal>
2017-04-21 22:33:02 +0000

    First commit

--- helper.c
+++ helper.c
@@ -2,12 +2,12 @@
  * dan bernstein at comp.lang.c
  * http://www.cse.yorku.ca/~oz/hash.html
  */
-unsigned long hash(unsigned char *str) {
+unsigned long hash(const char *str) {
     unsigned long hash = 5381;
     int c;
 
     while (c = *str++)
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-    return hash;
+    return hash%65535;
 }
--- third-part/build_third.sh
+++ third-part/build_third.sh
@@ -4,6 +4,5 @@ cd cmake-3.8.0
 ./configure
 gmake -j `nproc`
 cd ..
-git submodule add https://github.com/aws/aws-sdk-cpp.git
 cd aws-sdk-cpp/
 ../cmake-3.8.0/bin/cmake -DBUILD_ONLY="monitoring" -DBUILD_SHARED_LIBS=false .
