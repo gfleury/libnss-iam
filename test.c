bd2dad10ebd15907f2e1c4b224732bce05a16a4c
gfleury <gfleury@ip-172-31-13-97.us-west-2.compute.internal>
2017-04-22 13:30:03 +0000

    First commit

---
+++ test.c
@@ -0,0 +1,21 @@
+//#include "headers.h" 
+#include <pwd.h>
+
+/**
+ * test iam users retrieve
+ */
+#ifdef TEST
+#include <string.h>
+int main(int argc, char** argv)
+{
+        char buffer[1024];
+        struct passwd p;
+        strcpy(buffer, argv[1]);
+
+        if(get_posix_iam_user(buffer, 1024, &p)) {
+                printf("%s (%d)\n", p.pw_name, p.pw_uid);
+		if(get_posix_iam_user_by_uid(13903, buffer, 1024, &p)) 
+                	printf("%s (%d)\n", p.pw_name, p.pw_uid);
+	}
+}
+#endif
